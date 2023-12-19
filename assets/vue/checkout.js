b332f2d8764df64a4592ff4af8ef9ef2ddc1323a
Aida Vehapi <ai261veh@htwg-konstanz.de>
2023-12-20 00:15:10 +0100

    every changes, newest search

--- assets/vue/checkout.js
+++ assets/vue/checkout.js
@@ -1,51 +1,75 @@
-// checkout.js
 export default {
   data() {
     return {
       orderSummary: [],
+      totalQuantity: 0,
+      totalPrice: 0,
+      basket: [],
     };
   },
   created() {
-    // Annahme: Du hast bereits Artikelinformationen im basket
-    this.calculateOrderSummary();
+    this.loadBasket();
   },
   methods: {
+    loadBasket() {
+      let url = new URL(origin + "/api/basket");
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          this.basket = data.basket;
+          this.calculateOrderSummary();
+        })
+        .catch((error) => {
+          console.error("Fehler beim Laden des Warenkorbs:", error);
+        });
+    },
     calculateOrderSummary() {
-      // Hier kannst du die Logik hinzufügen, um die Bestellung zusammenzufassen
-      // Zum Beispiel, indem du die Gesamtmenge und den Gesamtpreis berechnest
-      // Annahme: basket ist ein Array von Artikeln mit name und price Eigenschaften
-      this.orderSummary = this.basket.map((item) => ({
+      const grouped = {};
+      this.basket.forEach((item) => {
+        if (!grouped[item.id]) {
+          grouped[item.id] = { ...item, quantity: 1 };
+        } else {
+          grouped[item.id].quantity++;
+        }
+      });
+
+      this.orderSummary = Object.values(grouped).map((item) => ({
         name: item.name,
         price: item.price,
+        quantity: item.quantity,
       }));
 
-      // Beispiel für die Berechnung der Gesamtmenge und des Gesamtpreises
-      // Hier musst du deine eigene Logik basierend auf deinen Anforderungen implementieren
-      // Annahme: Du hast eine Funktion calculateTotal() implementiert
-      // this.totalQuantity = this.calculateTotal('quantity');
-      // this.totalPrice = this.calculateTotal('price');
+      this.totalPrice = this.calculateTotal("price");
     },
-    // Hier kannst du weitere Methoden für den Checkout hinzufügen
-    // Zum Beispiel, um die Bestellung abzuschließen und den Benutzer weiterzuleiten
-    // oder um Änderungen an der Bestellung vorzunehmen
+    calculateTotal(property) {
+      return this.orderSummary.reduce(
+        (total, item) => total + item[property] * item.quantity,
+        0
+      );
+    },
   },
+
   template: `
-      <div class="container">
-        <h2 class="mt-4">Ihre Bestellzusammenfassung</h2>
-        <table class="table mt-4">
-          <tr v-for="(item, index) in orderSummary">
-            <td class="menu-title">
-              {{ item.name }}
-            </td>
-            <td class="menu-price">
-              {{ item.price }} &euro;
-            </td>
-          </tr>
-        </table>
-        <p>Total Quantity: {{ totalQuantity }}</p>
-        <p>Total Price: {{ totalPrice }} &euro;</p>
-        <router-link class="btn btn-secondary mr-5" to="/">Weiter einkaufen</router-link>
-        <router-link class="btn btn-primary" to="/checkout">Bestellung abschließen</router-link>
-      </div>
-    `,
+    <div class="container">
+      <h2 class="mt-4">Order Summary</h2>
+      <table class="table mt-4">
+        <tr v-for="(item, index) in orderSummary" :key="index">
+          <td class="menu-title">
+            {{ item.name }}
+          </td>
+          <td class="menu-price">
+            {{ item.price }} &euro;
+          </td>
+          <td class="menu-quantity">
+            Menge: {{ item.quantity }}
+          </td>
+        </tr>
+      </table>
+      <p>Total Price: {{ totalPrice }} &euro;</p>
+      <v-row justify="center" class="mt-4">
+        <v-btn  class="standard-btn" rounded="xl" to="/">Continue Shopping</v-btn>
+        <v-btn class="standard-btn"  color="#8d6e63" rounded="xl" to="/verifiedOrder" ><span style="color:white">Verify Order</span></v-btn>
+        </v-row> 
+    </div>
+  `,
 };
---
+++ assets/vue/verifiedOrder.js
@@ -0,0 +1,16 @@
+export default {
+  data() {
+    return {};
+  },
+
+  template: `
+  <div class="container">
+  <div style="text-align: center; font-size: 24px; font-weight:bold; margin-top: 50px;">
+    Thank you for your Order!
+  </div>
+    <div style="text-align: center; font-size: 24px; margin-top: 50px;">
+      See you next time..
+    </div>
+</div>
+`,
+};
